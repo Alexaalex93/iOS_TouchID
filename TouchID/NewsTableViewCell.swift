9786f4e1420c9d82fcdff5461fa6616ccd3d3638
355 Berry Street <p.mateo@355berrystreet.com>
2017-03-27 01:16:17 +0200

    Initial Commit

---
+++ TouchID/NewsTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  NewsTableViewCell.swift
+//  TouchID
+//
+//  Created by Pablo Mateo Fernández on 02/02/2017.
+//  Copyright © 2017 355 Berry Street S.L. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet var postImageView:UIImageView!
+    @IBOutlet var postTitle:UILabel!
+    @IBOutlet var postAuthor:UILabel!
+    @IBOutlet var authorImageView:UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+        
+        authorImageView.layer.cornerRadius = authorImageView.frame.width / 2
+        authorImageView.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
