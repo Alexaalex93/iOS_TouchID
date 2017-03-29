7e6306d39818e91efe698a31769c5bf4597f87a1
cice <cice@aula1PovMacN09.local>
2017-03-29 10:33:05 +0200

    Initial Commit

--- TouchID/LoginViewController.swift
+++ TouchID/LoginViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
-
+//Ir a la raiz, a build phases y añadir el framework en link binnary with libraries
+    //Local Authentication (Framework)
+        //LAContext
+            //canEvaluatePolicy
+            //evaluatePolicy   
+                //Muestra la ventana de dialogo para desbloquear el terminal  
     @IBOutlet weak var backgroundImageView:UIImageView!
     @IBOutlet weak var loginView:UIView!
     @IBOutlet weak var emailTextField:UITextField!
@@ -30,8 +36,9 @@ class LoginViewController: UIViewController {
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
         
-        showLoginDialog()
-
+        loginView.isHidden = true
+        authenticateWithTouchID()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,5 +60,68 @@ class LoginViewController: UIViewController {
         }, completion: nil)
         
     }
-
+    
+    func authenticateWithTouchID(){
+        //Authentication COntext
+        let localAuthContext = LAContext()
+        let razon = "Accede con TouchID a tu perfil"
+        var authError: NSError?
+        
+        if !localAuthContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &authError){
+            if let error = authError {
+                print(error.localizedDescription)
+            }
+            showLoginDialog()
+            return
+        }
+        //Identificar al usuario con TouchID
+        localAuthContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: razon) { (success, error) in
+            //Fallo al identificar
+            if !success{
+                if let error = error {
+                    switch error {
+                    case LAError.authenticationFailed:
+                        print("Authentication failed")
+                    case LAError.passcodeNotSet:
+                        print("Passcode not set")
+                    case LAError.systemCancel:
+                        print("Authentication canceled by system")
+                    case LAError.userCancel:
+                        print("Authentication canceled by user")
+                    case LAError.touchIDNotEnrolled:
+                        print("No hay info biométrica disponible")
+                    case LAError.touchIDNotAvailable:
+                        print("TouchID no disponible")
+                    case LAError.userFallback:
+                        print("Usuario prefiere usar contraseña")
+                    default:
+                        print(error.localizedDescription)
+                    }
+                }
+                // if!success lo hace en el backgroundThread y como esta en un hilo y queremos hacer cambios en la interfaz tenemos que salir al hilo principal con el operationQueue
+                //Ir hacia atrás al Login
+                OperationQueue.main.addOperation {
+                    self.showLoginDialog()
+                }
+            } else {
+                //Ha funcionado
+                print("Autenticacion ha funcionado")
+                OperationQueue.main.addOperation {
+                    self.performSegue(withIdentifier: "showHomeScreen", sender: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func authenticateWithPassword(){
+        if emailTextField.text == "pablo@gmail.com" && passwordTextField.text == "1234" {
+            performSegue(withIdentifier: "showHomeScreen", sender: nil)
+        } else {
+            //Shake
+            loginView.transform = CGAffineTransform(translationX: 25, y: 0)
+            UIView.animate(withDuration: 0.2, delay: 0.0, usingSpringWithDamping: 0.15, initialSpringVelocity: 0.3, options: .curveEaseInOut, animations: {
+                self.loginView.transform = CGAffineTransform.identity
+                }, completion: nil)
+        }
+    }
 }
